5990af9b5a6a3683d9a82903a581718eca12eef9
Sravanthi-Agge <sravanthiagge8@gmail.com>
2026-03-11 11:30:07 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -20,7 +20,7 @@ function Reg()
            // const res=await axios.post("http://localhost:8080/reg",data)
           const res=await axios.post(
             "https://cabsystemsms-1.onrender.com/register",
-            data
+            data)
         }
             alert(res.data)
         }
